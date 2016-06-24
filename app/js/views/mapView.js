c7885391d54ddcac1b48c060c0753bc47646d8a3
Clara Linos <clara.linos@gmail.com>
2016-06-24 19:45:13 +0200

    projection on robins with carto db and only carto db

--- app/js/views/mapView.js
+++ app/js/views/mapView.js
@@ -13,42 +13,48 @@ define([
     el: '#map',
 
     options: {
-      map: {
-        scrollWheelZoom: false,
-        continuousWorld: true,
-        worldCopyJump: false
+        basemap: 'https://api.tiles.mapbox.com/v4/goal16.9990f1b9/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZ29hbDE2IiwiYSI6ImNpcGgzaWwzbDAwMW52Mmt3ZG5tMnRwN3gifQ.-e8de3rW2J8gc2Iv3LzMnA',
+        map: {
+          center: [39.1, 4.5],
+          zoom: 2,
+          scrollWheelZoom: false,
+          continuousWorld: true,
+          worldCopyJump: false
+        },
+        cartodb: {
+          user_name: 'dhakelila',
+          css: '#score_test{ polygon-fill: #82bf72; polygon-opacity: 0.7; line-color: #f6faf9; line-width: 0.5; line-opacity: 1; }'
+        }
       },
-      cartodb: {
-        user_name: 'dhakelila',
-        css: '#score{ polygon-fill: #82bf72; polygon-opacity: 0.7; line-color: #f6faf9; line-width: 0.5; line-opacity: 1; }'
-      }
-    },
 
     initialize: function() {
       this._createMap();
     },
 
     _createMap: function() {
+      /* this is the definition for basemap */
+      var baseMap = L.tileLayer(this.options.basemap, {
+        attribution: '<a href="https://www.mapzen.com/rights">Attribution.</a>. Data &copy;<a href="https://openstreetmap.org/copyright">OSM</a> contributors.'
+      });
+
       /* Here we create the map with Leafleft... */
       this.map = L.map(this.el, this.options.map);
+      /* ...and we add the basemap layer with Leaflet as well */
+      this.map.addLayer(baseMap);
 
       this._activeLayer();
     },
 
     _activeLayer: function() {
       this._createLayer().done(_.bind(function() {
-        //We remove the previous layer just when the new one arrive.
-        //This way, we are sure we only have one layer at a time.
-
         this._addLayer();
-
       }, this));
     },
 
     _createLayer: function() {
       var sql = this._getLayerQuery();
       var cartoAccount = this.options.cartodb.user_name;
-      var cartoCss = this.options.cartodb.css || '#score{ polygon-fill: #FF6600; polygon-opacity: 0.7; line-color: #FFF; line-width: 0.5; line-opacity: 1; }';
+      var cartoCss = this.options.cartodb.css || '#table_score_test{ polygon-fill: #FF6600; polygon-opacity: 0.7; line-color: #FFF; line-width: 0.5; line-opacity: 1; }';
       var deferred = $.Deferred();
 
       var request = {
@@ -72,11 +78,7 @@ define([
       }).done(_.bind(function(data) {
             var tileUrl = 'http://'+ cartoAccount +'.cartodb.com/api/v1/map/'+ data.layergroupid + '/{z}/{x}/{y}.png32';
             this._removeLayer();
-            this.layer = L.tileLayer(tileUrl, {
-              maxZoom: 14,
-              minZoom: 0,
-              continuousWorld: true
-            });
+            this.layer = L.tileLayer(tileUrl, { noWrap: true });
             return deferred.resolve();
           }, this));
 
@@ -97,7 +99,7 @@ define([
 
     _getLayerQuery: function() {
 
-      var query = 'SELECT  cartodb_id, cartodb_georef_status, iso, score,  st_transform(st_makevalid(the_geom_webmercator),954030) as the_geom_webmercator FROM  score_test';
+       var query = 'SELECT  cartodb_id, cartodb_georef_status, iso, score,  st_transform(st_makevalid(the_geom_webmercator), 54030) as the_geom_webmercator FROM  score_test';
 
       return query;
     },
